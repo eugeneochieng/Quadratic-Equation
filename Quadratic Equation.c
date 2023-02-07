638fa3947912ae22d86a2460bda44d29c44d289b
eugeneochieng <123956751+eugeneochieng@users.noreply.github.com>
2023-02-07 15:57:12 +0300

    Add files via upload

---
+++ Quadratic Equation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float a, b, c;
+    float root1, root2;
+
+    printf("Enter a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    double d = b * b - 4 * a * c;
+
+    // condition for real and different roots
+    if (d > 0)
+    {
+        root1 = (-b + sqrt(d)) / (2 * a);
+        root2 = (-b - sqrt(d)) / (2 * a);
+        printf("root1 = %.2lf and root2 = %.2lf", root1, root2);
+    }
+
+    //condition for real and equal roots
+    else if (d == 0)
+    {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2lf;", root1);
+    }
+
+    // if roots are equal
